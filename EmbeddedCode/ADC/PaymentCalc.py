5c28a039cbd4e5d413513eb64b94df17ec21a8eb
Lillian Cordelia Gwendolyn <lilliangwendolyn@gmail.com>
2021-07-13 14:31:10 -0700

    Implemented one-hot style and ADC state machine - needs testing and work for inter-device communication

--- EmbeddedCode/ADC/PaymentCalc.py
+++ EmbeddedCode/ADC/PaymentCalc.py
@@ -11,66 +11,109 @@ import time
 #sys used for exiting and throwing errors
 import sys
 
-#init code taken from https://learn.adafruit.com/raspberry-pi-analog-to-digital-converters/mcp3008
+#MCP3008 init code taken from https://learn.adafruit.com/raspberry-pi-analog-to-digital-converters/mcp3008
+#note - software SPI works on any machine with pins plugged in correctly + correct files installed
+#but is slightly slower/bulkier than hardware SPI
+#hardware SPI requires enabling a few settings on the RP
+#but gives slightly better performance
+
 # Software SPI configuration:
 mcp = Adafruit_MCP3008.MCP3008(clk=constants.ADC_CLK_RP_PIN, cs=constants.ADC_CS_RP_PIN, miso=constants.ADC_MISO_RP_PIN, mosi=constants.ADC_MOSI_RP_PIN)
 
 # Hardware SPI configuration:
 # mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(constants.ADC_RP_SPI_PORT, constants.ADC_RP_SPI_DEVICE))
 
-#
+#background -
+#there is only one terminal, and only one tap can run at any given time
+#so essentially there is a reset between every user
+#thus we are essentially doing a one-hot version of the taps
+#instead of treating them as separate instances
 
 #TODO:
 #get formula so that do not need to keep track of every value taken
 #and instead constantly add to formula so that receiving devices
 #always get the current amnt of volume exported
 
-#boolean to see if we use them, right now we only use one tap
-#will eventually be managed by an exterior thread instead of handled manually
-CurrentTap = 0
-WF_TAP_ONE_ON = True
-WF_TAP_TWO_ON = False
-WF_TAP_THREE_ON = False
-WF_TAP_FOUR_ON = False
+#enums for state
+currTap = constants.WF_TAP.ONE
+currState = constants.WF_STATE.WAITING_FOR_CUSTOMER
+#time spent running current tap - used to track if timeout should occur
+timeRunning = 0
+#reading from 0 to 1024
+currReading = 0
+#tracking how much volume has been exported over this pour
+totalVolOutput = 0
 
-#create 4 empty lists to store speeds taken over pouring
-WF_TAP_ONE_Speed_List = 0 * constants.LIST_SIZE
-WF_TAP_TWO_Speed_List = 0 * constants.LIST_SIZE
-WF_TAP_THREE_Speed_List = 0 * constants.LIST_SIZE
-WF_TAP_FOUR_Speed_List = 0 * constants.LIST_SIZE
+def RunState_ERROR():
+   print("Wonderfil has encountered an error\n")
+   time.sleep(5)
+   return
 
-#individual indeces for each speed list
-WF_TAP_ONE_List_Index = 0
-WF_TAP_TWO_List_Index = 0
-WF_TAP_THREE_List_Index = 0
-WF_TAP_FOUR_List_Index = 0
+def RunState_WAITING_FOR_CUSTOMER():
+   #if signal currState = constants.WF_STATE.TAP_SELECTED
+   time.sleep(1)
+   return
+
+def RunState_TAP_SELECTED():
+   #get tap from exterior signal
+   currTap = constants.WF_TAP.ONE
+   #init things
+   timeRunning = 0
+   totalVolOutput = 0
+   currState = constants.WF_STATE.READY_FOR_POUR
+   return
+
+def RunState_READY_FOR_POUR():
+   #look for first point when adc gets a signal
+   currReading = mcp.read_adc(currTap)
+   if(currReading == 0):
+      #wait one interval and repeat the check
+      time.sleep(constants.SAMPLE_INTERVAL)
+      return
+   #else do math to add reading to total vol output
+   #
+   #
+   time.sleep(constants.SAMPLE_INTERVAL)
+   timeRunning += constants.SAMPLE_INTERVAL
+   currState = constants.WF_STATE.RUNNING
+   return
+
+def RunState_RUNNING():
+   currReading = mcp.read_adc(currTap)
+   if((currReading == 0) or (timeRunning >= constants.constants.MAX_TIMEOUT)):
+      currState = constants.WF_STATE.POUR_COMPLETED
+      return
+   #else do math to add reading to total vol output
+   #
+   #
+   time.sleep(constants.SAMPLE_INTERVAL)
+   timeRunning += constants.SAMPLE_INTERVAL
+   return
+
+def RunState_POUR_COMPLETED():
+   #send info to exterior program
+   #check for errors
+   currState = constants.WF_STATE.WAITING_FOR_CUSTOMER
+   return
+
+#python doesnt have built in switch case statements
+#so instead its a dictionary of functions?
+ADC_SM = {
+   constants.WF_STATE.ERROR: RunState_ERROR,
+   constants.WF_STATE.WAITING_FOR_CUSTOMER: RunState_WAITING_FOR_CUSTOMER,
+   constants.WF_STATE.TAP_SELECTED: RunState_TAP_SELECTED,
+   constants.WF_STATE.READY_FOR_POUR: RunState_READY_FOR_POUR,
+   constants.WF_STATE.RUNNING: RunState_RUNNING,
+   constants.WF_STATE.POUR_COMPLETED: RunState_POUR_COMPLETED
+}
+
 
 #task loop
 while True:
 
-    if(constants.WF_TAP_ONE_ON and (WF_TAP_ONE_List_Index < constants.LIST_SIZE)):
-
-       WF_TAP_ONE_Speed_List[WF_TAP_ONE_List_Index] = mcp.read_adc(constants.WF_TAP_ONE_ADC_PIN)
-       ++WF_TAP_ONE_List_Index
-
-    if(constants.WF_TAP_TWO_ON and (WF_TAP_TWO_List_Index < constants.LIST_SIZE)):
-        
-       WF_TAP_TWO_Speed_List[WF_TAP_TWO_List_Index] = mcp.read_adc(constants.WF_TAP_TWO_ADC_PIN)
-       ++WF_TAP_TWO_List_Index
-
-    if(constants.WF_TAP_THREE_ON and (WF_TAP_THREE_List_Index < constants.LIST_SIZE)):
-        
-       WF_TAP_THREE_Speed_List[WF_TAP_THREE_List_Index] = mcp.read_adc(constants.WF_TAP_THREE_ADC_PIN)
-       ++WF_TAP_THREE_List_Index
-
-    if(constants.WF_TAP_FOUR_ON and (WF_TAP_FOUR_List_Index < constants.LIST_SIZE)):
-        
-       WF_TAP_FOUR_Speed_List[WF_TAP_FOUR_List_Index] = mcp.read_adc(constants.WF_TAP_FOUR_ADC_PIN)
-       ++WF_TAP_FOUR_List_Index
-
-    #send all info to payment terminal every few cycles - needs math of everything + curr duration of tap
-    
-    time.sleep(constants.SAMPLE_INTERVAL)
+   ADC_SM[currState]()
+   
+   time.sleep(constants.SAMPLE_INTERVAL)
 
 #will never be reached, here for propriety
 #empty means exit success / return 0
--- EmbeddedCode/ADC/constants.py
+++ EmbeddedCode/ADC/constants.py
@@ -1,18 +1,40 @@
 #Constants used for payment calc / equivalent of #defines in c
 #Lillian Cordelia Gwendolyn 07/13/2021 @ Wonderfil
 
+#enum not strictly required but useful for organization
+import enum
+
 #timing constants
-SAMPLE_INTERVAL = 0.01 #100x/sec
+SAMPLE_INTERVAL = 0.05 #20x/sec
 MAX_TIMEOUT = 30 #30 sec max
 
 LIST_SIZE = (MAX_TIMEOUT / SAMPLE_INTERVAL)
 
+'''
 #ports of ADC chip used for each tap
-#0-7 correspond to pins 0-7 on the chip
 WF_TAP_ONE_ADC_PIN = 0
 WF_TAP_TWO_ADC_PIN = 2
 WF_TAP_THREE_ADC_PIN = 4
 WF_TAP_FOUR_ADC_PIN = 6
+'''
+
+#enum to organize by tap numbers so i dont need to use numbers
+#doubles as port used for ADC chip reading
+#0-7 correspond to pins 0-7 on the chip
+class WF_TAP(enum.Enum):
+    NONE = -1
+    ONE = 0
+    TWO = 2
+    THREE = 4
+    FOUR = 6
+
+class WF_STATE(enum.Enum):
+    ERROR = -1
+    WAITING_FOR_CUSTOMER = 0
+    TAP_SELECTED = 1
+    READY_FOR_POUR = 2
+    RUNNING = 3
+    POUR_COMPLETED = 4
 
 #ports of Raspberry Pi used to drive ADC
 #taken from https://learn.adafruit.com/raspberry-pi-analog-to-digital-converters/mcp3008
