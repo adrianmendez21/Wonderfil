919ed68cfc22266d9c2fd61b3979efde85a66cb8
Lillian Cordelia Gwendolyn <lilliangwendolyn@gmail.com>
2021-07-14 11:28:11 -0700

    slight changes to scope of variables

--- EmbeddedCode/ADC/PaymentCalc.py
+++ EmbeddedCode/ADC/PaymentCalc.py
@@ -1,28 +1,12 @@
 #Payment Calculator using Raspberry PI and MCP3008 ADC Chip
-#Lillian Cordelia Gwendolyn 07/13/2021 @ Wonderfil
-
-import EmbeddedCode.ADC.constants as constants
-
-# Import SPI library (for hardware SPI) and MCP3008 library.
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_MCP3008
+#Lillian Cordelia Gwendolyn 07/14/2021 @ Wonderfil
 
 #time used for delaying current thread to create sampling interval
-import time
+from time import sleep
 #sys used for exiting and throwing errors
-import sys
+from sys import exit
 
-#MCP3008 init code taken from https://learn.adafruit.com/raspberry-pi-analog-to-digital-converters/mcp3008
-#note - software SPI works on any machine with pins plugged in correctly + correct files installed
-#but is slightly slower/bulkier than hardware SPI
-#hardware SPI requires enabling a few settings on the RP
-#but gives slightly better performance
-
-# Software SPI configuration:
-mcp = Adafruit_MCP3008.MCP3008(clk=constants.ADC_CLK_RP_PIN, cs=constants.ADC_CS_RP_PIN, miso=constants.ADC_MISO_RP_PIN, mosi=constants.ADC_MOSI_RP_PIN)
-
-# Hardware SPI configuration:
-# mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(constants.ADC_RP_SPI_PORT, constants.ADC_RP_SPI_DEVICE))
+import EmbeddedCode.ADC.constants as constants
 
 #background -
 #there is only one terminal, and only one tap can run at any given time
@@ -49,15 +33,19 @@ totalVolOutput = 0
 
 def RunState_ERROR():
    print("Wonderfil has encountered an error\n")
-   time.sleep(5)
+   sleep(5)
    return
 
 def RunState_WAITING_FOR_CUSTOMER():
    #if signal currState = constants.WF_STATE.TAP_SELECTED
-   time.sleep(1)
+   sleep(1)
    return
 
 def RunState_TAP_SELECTED():
+   global currTap
+   global timeRunning
+   global totalVolOutput
+   global currState
    #get tap from exterior signal
    currTap = constants.WF_TAP.ONE
    #init things
@@ -67,35 +55,43 @@ def RunState_TAP_SELECTED():
    return
 
 def RunState_READY_FOR_POUR():
+   #global currReading
+   global timeRunning
+   global currState
    #look for first point when adc gets a signal
-   currReading = mcp.read_adc(currTap)
+   currReading = constants.mcp.read_adc(currTap)
    if(currReading == 0):
       #wait one interval and repeat the check
-      time.sleep(constants.SAMPLE_INTERVAL)
+      sleep(constants.SAMPLE_INTERVAL)
       return
    #else do math to add reading to total vol output
    #
    #
-   time.sleep(constants.SAMPLE_INTERVAL)
+   sleep(constants.SAMPLE_INTERVAL)
    timeRunning += constants.SAMPLE_INTERVAL
    currState = constants.WF_STATE.RUNNING
    return
 
 def RunState_RUNNING():
-   currReading = mcp.read_adc(currTap)
+   #global currReading
+   global timeRunning
+   global currState
+   currReading = constants.mcp.read_adc(currTap)
    if((currReading == 0) or (timeRunning >= constants.constants.MAX_TIMEOUT)):
       currState = constants.WF_STATE.POUR_COMPLETED
       return
    #else do math to add reading to total vol output
    #
    #
-   time.sleep(constants.SAMPLE_INTERVAL)
+   sleep(constants.SAMPLE_INTERVAL)
    timeRunning += constants.SAMPLE_INTERVAL
    return
 
 def RunState_POUR_COMPLETED():
    #send info to exterior program
    #check for errors
+   #global timeRunning
+   global currState
    currState = constants.WF_STATE.WAITING_FOR_CUSTOMER
    return
 
@@ -116,7 +112,7 @@ while True:
 
    ADC_SM[currState]()
    
-   time.sleep(constants.SAMPLE_INTERVAL)
+   sleep(constants.SAMPLE_INTERVAL)
 
 #will never be reached, here for propriety
 #empty means exit success / return 0
--- EmbeddedCode/ADC/constants.py
+++ EmbeddedCode/ADC/constants.py
@@ -1,14 +1,41 @@
 #Constants used for payment calc / equivalent of #defines in c
-#Lillian Cordelia Gwendolyn 07/13/2021 @ Wonderfil
+#Lillian Cordelia Gwendolyn 07/14/2021 @ Wonderfil
 
 #enum not strictly required but useful for organization
 import enum
+
+# Import SPI library (for hardware SPI) and MCP3008 library.
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+#MCP3008 init code taken from https://learn.adafruit.com/raspberry-pi-analog-to-digital-converters/mcp3008
+#note - software SPI works on any machine with pins plugged in correctly + correct files installed
+#but is slightly slower/bulkier than hardware SPI
+#hardware SPI requires enabling a few settings on the RP
+#but gives slightly better performance
+
+#ports of Raspberry Pi used to drive ADC
+#taken from https://learn.adafruit.com/raspberry-pi-analog-to-digital-converters/mcp3008
+#ports for software-run SPI
+ADC_CLK_RP_PIN = 18
+ADC_MISO_RP_PIN = 23
+ADC_MOSI_RP_PIN = 24
+ADC_CS_RP_PIN = 25
+#ports for hardware-run SPI
+ADC_RP_SPI_PORT = 0
+ADC_RP_SPI_DEVICE = 0
+
+# Software SPI configuration:
+mcp = Adafruit_MCP3008.MCP3008(clk=ADC_CLK_RP_PIN, cs=ADC_CS_RP_PIN, miso=ADC_MISO_RP_PIN, mosi=ADC_MOSI_RP_PIN)
+
+# Hardware SPI configuration:
+# mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(ADC_RP_SPI_PORT, ADC_RP_SPI_DEVICE))
 
 #timing constants
 SAMPLE_INTERVAL = 0.05 #20x/sec
 MAX_TIMEOUT = 30 #30 sec max
 
-LIST_SIZE = (MAX_TIMEOUT / SAMPLE_INTERVAL)
+#LIST_SIZE = (MAX_TIMEOUT / SAMPLE_INTERVAL)
 
 '''
 #ports of ADC chip used for each tap
@@ -35,14 +62,3 @@ class WF_STATE(enum.Enum):
     READY_FOR_POUR = 2
     RUNNING = 3
     POUR_COMPLETED = 4
-
-#ports of Raspberry Pi used to drive ADC
-#taken from https://learn.adafruit.com/raspberry-pi-analog-to-digital-converters/mcp3008
-#ports for software-run SPI
-ADC_CLK_RP_PIN = 18
-ADC_MISO_RP_PIN = 23
-ADC_MOSI_RP_PIN = 24
-ADC_CS_RP_PIN = 25
-#ports for hardware-run SPI
-ADC_RP_SPI_PORT = 0
-ADC_RP_SPI_DEVICE = 0
